7fa4f05c856a1a19ba5694f7e98ba1b161679ce1
John Doe <johnshivjo@gmail.com>
2023-01-31 18:31:02 -0500

    Fixed naming conventions

--- ShivitoMGO_UWP/DataModels.cs
+++ ShivitoMGO_UWP/DataModels.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 
 namespace DataModels
 {
-    class Player
+    public class Custom
     {
-        public int id { get; set; }
-        public string name { get; set; }
-        public int rank { get; set; }
-        public bool host { get; set; }
-        public string emblem { get; set; }
+        public bool suppressor { get; set; }
+        public bool gp30 { get; set; }
+        public bool xm320 { get; set; }
+        public bool masterkey { get; set; }
+        public bool scope { get; set; }
+        public bool sight { get; set; }
+        public bool laser { get; set; }
+        public bool lighthg { get; set; }
+        public bool lightlg { get; set; }
+        public bool grip { get; set; }
     }
 
-    class Lobby
+    public class Data
+    {
+        public int players { get; set; }
+        public List<Lobby> lobbies { get; set; }
+    }
+
+    public class Items
+    {
+        public bool envg { get; set; }
+        public bool drum { get; set; }
+    }
+
+    public class LevelLimit
+    {
+        public bool enabled { get; set; }
+        public int @base { get; set; }
+        public int tolerance { get; set; }
+    }
+
+    public class Lobby
     {
         public int id { get; set; }
         public int lobbyId { get; set; }
@@ -24,19 +48,113 @@ namespace DataModels
         public List<List<int>> games { get; set; }
         public string comment { get; set; }
         public string location { get; set; }
+        public Settings settings { get; set; }
     }
 
-    class Data
+    public class Player
     {
-        public int players { get; set; }
-        public List<Lobby> lobbies { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int rank { get; set; }
+        public bool host { get; set; }
+        public string emblem { get; set; }
     }
 
-    class Root
+    public class Primary
+    {
+        public bool vz { get; set; }
+        public bool p90 { get; set; }
+        public bool mp5 { get; set; }
+        public bool patriot { get; set; }
+        public bool ak { get; set; }
+        public bool m4 { get; set; }
+        public bool mk17 { get; set; }
+        public bool xm8 { get; set; }
+        public bool g3a3 { get; set; }
+        public bool svd { get; set; }
+        public bool mosin { get; set; }
+        public bool m14 { get; set; }
+        public bool vss { get; set; }
+        public bool dsr { get; set; }
+        public bool m870 { get; set; }
+        public bool saiga { get; set; }
+        public bool m60 { get; set; }
+        public bool shield { get; set; }
+        public bool rpg { get; set; }
+        public bool knife { get; set; }
+    }
+
+    public class Root
     {
         public bool success { get; set; }
         public Data data { get; set; }
-        public string message { get; set; }
+    }
+
+    public class Secondary
+    {
+        public bool gsr { get; set; }
+        public bool mk2 { get; set; }
+        public bool @operator { get; set; }
+        public bool g18 { get; set; }
+        public bool mk23 { get; set; }
+        public bool de { get; set; }
+    }
+
+    public class Settings
+    {
+        public bool dedicated { get; set; }
+        public int maxPlayers { get; set; }
+        public int briefingTime { get; set; }
+        public bool nonStat { get; set; }
+        public bool friendlyFire { get; set; }
+        public bool autoAim { get; set; }
+        public Uniques uniques { get; set; }
+        public bool enemyNametags { get; set; }
+        public bool silentMode { get; set; }
+        public bool autoAssign { get; set; }
+        public bool teamsSwitch { get; set; }
+        public bool ghosts { get; set; }
+        public LevelLimit levelLimit { get; set; }
+        public bool voiceChat { get; set; }
+        public int teamKillKick { get; set; }
+        public int idleKick { get; set; }
+        public WeaponRestrictions weaponRestrictions { get; set; }
+    }
+
+    public class Support
+    {
+        public bool grenade { get; set; }
+        public bool wp { get; set; }
+        public bool stun { get; set; }
+        public bool chaff { get; set; }
+        public bool smoke { get; set; }
+        public bool smoke_r { get; set; }
+        public bool smoke_g { get; set; }
+        public bool smoke_y { get; set; }
+        public bool eloc { get; set; }
+        public bool claymore { get; set; }
+        public bool sgmine { get; set; }
+        public bool c4 { get; set; }
+        public bool sgsatchel { get; set; }
+        public bool magazine { get; set; }
+    }
+
+    public class Uniques
+    {
+        public bool enabled { get; set; }
+        public bool random { get; set; }
+        public int red { get; set; }
+        public int blue { get; set; }
+    }
+
+    public class WeaponRestrictions
+    {
+        public bool enabled { get; set; }
+        public Primary primary { get; set; }
+        public Secondary secondary { get; set; }
+        public Support support { get; set; }
+        public Custom custom { get; set; }
+        public Items items { get; set; }
     }
 }
 
--- ShivitoMGO_UWP/MainPage.xaml.cs
+++ ShivitoMGO_UWP/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Collections;
 using Windows.Devices.Printers;
 using Windows.UI.Core;
+using System.Threading.Tasks;
 
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -27,33 +28,39 @@ namespace ShivitoMGO_UWP
         {
             this.InitializeComponent();
 
-
-
-            /*            listOfStudents.Add(new Student { Name = "John", Age = 20 });
-                        listOfStudents.Add(new Student { Name = "Bob", Age = 21 });
-                        listOfStudents.Add(new Student { Name = "Steve", Age = 19 });
-                        listOfStudents.Add(new Student { Name = "Marko", Age = 18 });
-                        listOfStudents.Add(new Student { Name = "Igor", Age = 20 });
-                        listOfStudents.Add(new Student { Name = "Ivan", Age = 20 });
-                        listOfStudents.Add(new Student { Name = "Nina", Age = 21 });
-                        listOfStudents.Add(new Student { Name = "Paul", Age = 20 });
-                        listOfStudents.Add(new Student { Name = "Ana", Age = 23 });
-                        listOfStudents.Add(new Student { Name = "Ivana", Age = 20 });*/
-            Form1_Load();
+            try { form1Load(); }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                listOfStudents.Add(new Student { Name = "No Internet", Num_Of_Players = 0, Max_Players = 0 });
+            }
             mylist.ItemsSource = listOfStudents;
-            mylist.ItemClick += Mylist_ItemClick;
+            mylist.ItemClick += MylistItemClick;
             TTimer = new Timer(
-                new TimerCallback(TickTimer),
-                null,
-                5000,
-                5000);
+    TickTimerAsync,
+    null,
+    5000,
+    5000);
 
 
         }
 
-        public void TickTimer(object state)
+        private TimerCallback TickTimerAsync()
         {
-            bool list_needs_updated = Check_mgo_values();
+            throw new NotImplementedException();
+        }
+
+        public async void TickTimerAsync(object state)
+        {
+            bool list_needs_updated = true;
+            try
+            {
+                list_needs_updated = await CheckMgoValuesAsync();
+            }catch(Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+            
             if (!list_needs_updated) {
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                 Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
@@ -62,7 +69,12 @@ namespace ShivitoMGO_UWP
     mylist.ItemsSource = null;
     mylist.Items.Clear();
     System.Diagnostics.Debug.WriteLine("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
-    Form1_Load();
+    try { form1Load(); }
+    catch (Exception ex)
+    {
+        System.Diagnostics.Debug.WriteLine(ex.Message);
+        listOfStudents.Add(new Student { Name = "No Internet", Num_Of_Players = 0, Max_Players = 0 });
+    }
     mylist.ItemsSource = listOfStudents;
 });
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
@@ -77,23 +89,25 @@ namespace ShivitoMGO_UWP
     Timeout.Infinite);
         }
 
-        private void Mylist_ItemClick(object sender, ItemClickEventArgs e)
+        private void MylistItemClick(object sender, ItemClickEventArgs e)
         {
             
             var clickedItem = e.ClickedItem;
             if (clickedItem != null) { System.Diagnostics.Debug.WriteLine(clickedItem.ToString()); }
             mylist.ItemsSource = null;
             mylist.Items.Clear();
-            Form1_Load();
+            try { form1Load(); } catch (Exception ex) { System.Diagnostics.Debug.WriteLine(ex.Message);
+                listOfStudents.Add(new Student { Name = "No Internet", Num_Of_Players = 0, Max_Players = 0 });
+            }
             mylist.ItemsSource = listOfStudents;
         }
 
-        public Boolean Check_mgo_values()
+        public async Task<bool> CheckMgoValuesAsync()
         {
             checklistupdate.Clear();
             using (var httpClient = new HttpClient())
             {
-                var response = httpClient.GetStringAsync("https://mgo2pc.com/api/v1/games").Result;
+                var response = await httpClient.GetStringAsync("https://mgo2pc.com/api/v1/games?extra=true");
                 var root = JsonConvert.DeserializeObject<Root>(response);
                 foreach (var lobby in root.data.lobbies)
                 {
@@ -136,12 +150,12 @@ namespace ShivitoMGO_UWP
             public int Max_Players { get; set; }
         }
 
-        private void Form1_Load()
+        private void form1Load()
         {
             listOfStudents.Clear();
             using (var httpClient = new HttpClient())
             {
-                var response = httpClient.GetStringAsync("https://mgo2pc.com/api/v1/games").Result;
+                var response = httpClient.GetStringAsync("https://mgo2pc.com/api/v1/games?extra=true").Result;
                 var root = JsonConvert.DeserializeObject<Root>(response);
                 foreach (var lobby in root.data.lobbies)
                 {
